5d5012b33f3e26e26ceae4ed6437bd8a464f6184
Neha Chandran <67389701+mastermercury18@users.noreply.github.com>
2025-05-15 22:37:53 -0700

    Add files via upload

---
+++ 06 Sorting Widgets/Widget.java
@@ -0,0 +1,71 @@
+// Name:
+// Date: 
+
+public class Widget implements Comparable<Widget>
+{
+   //fields
+   private int cubits;
+   private int hands;
+   //constructors
+   
+   public Widget(){
+      cubits = 0;
+      hands = 0;
+   }
+   public Widget(int c, int h){
+      cubits = c;
+      hands = h;
+   }
+   public Widget(Widget w){
+      cubits = w.getCubits();
+      hands = w.getHands();
+   }
+   
+   //accessors and modifiers
+   public int getCubits(){
+      return cubits;
+   }
+   public int getHands(){
+      return hands;
+   }
+   public void setCubits(int c){
+      cubits = c;
+   }
+   
+   public void setHands(int h){
+      hands = h;
+   }
+   
+   //compareTo and equals
+   public int compareTo(Widget w){
+      if (w.cubits > cubits)
+         return -1;
+      else if (w.cubits == cubits){
+         if (w.hands > hands)
+            return -1;
+         else if (w.hands < hands)
+            return 1;
+         else
+            return 0;
+      }
+      else{
+         return 1;
+      }
+   }
+   
+   public boolean equals(Object w){
+      Widget v;
+      //System.out.println(this + ", " + w);
+      v = (Widget) w;
+      if (v.compareTo(this) == 0)
+         return true;
+      return false;
+   }
+
+   //toString
+   
+   public String toString(){
+      return cubits + " cubits " + hands + " hands ";
+   }
+   
+}
